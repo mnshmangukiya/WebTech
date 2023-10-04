7b434becf8d109e0a32bfc202e7e7e782edd7e99
mnshmangukiya <mnshmangukiya@gmail.com>
2023-10-04 12:08:26 +0530

    first commit

---
+++ src/db/conn.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+// creating a database 
+mongoose.connect("mongodb://127.0.0.1:27017/ManishMangukiya", {
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(() => {
+    console.log("connection successful");
+}).catch((error) => {
+    console.log(error);
+})
